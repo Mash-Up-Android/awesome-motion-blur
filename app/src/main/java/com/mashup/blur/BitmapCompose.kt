38b2efb768bd11154767444b634914baff7415a0
gjeon <gjeon@gmarket.com>
2024-05-19 14:46:36 +0900

    오른쪽 이동 오류 제거

--- app/src/main/java/com/mashup/blur/BitmapCompose.kt
+++ app/src/main/java/com/mashup/blur/BitmapCompose.kt
@@ -6,16 +6,12 @@ import kotlin.math.max
 fun composeLeftTopBitmap(
     baseBitmap: Bitmap,
     composeBitmap: Bitmap,
-    cropRemainArea: Boolean = true  // false : 중첩되어 더해지지 않는 영역 제거
+    cropRemainArea: Boolean = true  // false : 중첩되어 더해지지 않는 영역 유지
 ): Bitmap {
     val newBitmap = if (cropRemainArea.not()) {
         baseBitmap.copy(baseBitmap.config, true)
     } else {
-        Bitmap.createBitmap(
-            max(composeBitmap.width, composeBitmap.width),
-            max(composeBitmap.height, composeBitmap.height),
-            composeBitmap.config
-        )
+        Bitmap.createBitmap(composeBitmap.width, composeBitmap.height, composeBitmap.config)
     }
 
     for (i in 0 until composeBitmap.width) {
@@ -33,29 +29,23 @@ fun composeLeftTopBitmap(
 fun composeRightBottomBitmap(
     baseBitmap: Bitmap,
     composeBitmap: Bitmap,
-    cropRemainArea: Boolean = false  // false : 중첩되어 더해지지 않는 영역 제거
+    cropRemainArea: Boolean = true  // false : 중첩되어 더해지지 않는 영역 유지
+    // Todo : 이거 이상함. true 일 때 다시 처리
 ): Bitmap {
     val newBitmap = if (cropRemainArea.not()) {
         baseBitmap.copy(baseBitmap.config, true)
     } else {
-        Bitmap.createBitmap(
-            max(composeBitmap.width, composeBitmap.width),
-            max(composeBitmap.height, composeBitmap.height),
-            composeBitmap.config
-        )
+        Bitmap.createBitmap(composeBitmap.width, composeBitmap.height, composeBitmap.config)
     }
 
     val diffWidth = baseBitmap.width - composeBitmap.width
     val diffHeight = baseBitmap.height - composeBitmap.height
-    for (i in newBitmap.width - 1 downTo diffWidth) {
-        for (j in newBitmap.height - 1 downTo diffHeight) {
+    for (i in 0 until composeBitmap.width) {
+        for (j in 0 until composeBitmap.height) {
             newBitmap.setPixel(
                 i,
                 j,
-                baseBitmap.getPixel(i, j) compose composeBitmap.getPixel(
-                    i - diffWidth,
-                    j - diffHeight
-                )
+                baseBitmap.getPixel(i + diffWidth, j + diffHeight) compose composeBitmap.getPixel(i, j)
             )
         }
     }
